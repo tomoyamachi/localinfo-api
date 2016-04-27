e220507396ec0350cc62805343e0192a218acb77
Tomoya AMACHI <tomoya.amachi@gmail.com>
2016-04-27 08:24:02 +0000

    簡易的なテストを作成

--- app/v1/controllers/TreasureController.php
+++ app/v1/controllers/TreasureController.php
@@ -15,8 +15,8 @@ class TreasureController extends \Api\Controllers\Api\AbstractController
 
         // 引数に問題がなければ検索
         $result = [['id' => 1, 'name' => 'hoge']];
-        /* $products = Product::find($params); */
-        /* $result = RProduct::getMultipleContent($products); */
+        /* $treasures = Treasure::find($params); */
+        /* $result = RTreasure::getMultipleContent($treasures); */
         return $this->responseValidStatus($result);
     }
 
@@ -25,18 +25,19 @@ class TreasureController extends \Api\Controllers\Api\AbstractController
      */
     public function getTargetAction()
     {
-        $productId = $this->dispatcher->getParam('product_id');
-        $response = $this->checkPositiveInteger($productId);
+        $treasureId = $this->dispatcher->getParam('treasure_id');
+        $response = $this->checkPositiveInteger($treasureId);
         if ($response !== true) {
             return;
         }
+        $result = ['id' => 1, 'name' => 'hoge'];
 
-        try {
-            $product = Product::findFirst($productId);
-            $result = RProduct::getContent($product);
-        } catch (\Exception $e) {
-            return $this->responseExceptionError($e);
-        }
+        /* try { */
+        /*     $treasure = Treasure::findFirst($treasureId); */
+        /*     $result = RTreasure::getContent($treasure); */
+        /* } catch (\Exception $e) { */
+        /*     return $this->responseExceptionError($e); */
+        /* } */
 
         return $this->responseValidStatus($result);
     }
