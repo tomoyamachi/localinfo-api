8bd3e97b0995c0777fa0cacf9efa46699ff8f9a1
Tomoya AMACHI <tomoya.amachi@gmail.com>
2016-06-09 10:41:47 +0000

    metadataをapcに持たせる

--- config/config.d/service.d/modelsMetadata.php
+++ config/config.d/service.d/modelsMetadata.php
@@ -1,9 +1,16 @@
 <?php
 
 use Phalcon\DI;
-use Phalcon\Mvc\Model\MetaData\Memory as ModelsMetadata;
+use Phalcon\Mvc\Model\MetaData\Apc as ApcMetaData;
 
 $di = DI::getDefault();
 $di->set('modelsMetadata', function () use ($di) {
-    return new ModelsMetadata();
+    // Create a meta-data manager with APC
+    $metaData = new ApcMetaData(
+        array(
+            "lifetime" => 86400,
+            "prefix"   => "lapi-metadata"
+        )
+    );
+    return $metaData;
 });
