a662ec865f1820399660b2e0a68d9122e0cbfdbf
Tomoya AMACHI <tomoya.amachi@gmail.com>
2016-07-01 08:40:03 +0000

    fixed setting

--- config/config.d/service.d/db.php
+++ config/config.d/service.d/db.php
@@ -17,12 +17,43 @@ $di->set('db', function () use ($di) {
     }
     $config = $di->get('config');
 
-    $descriptor = $config->database->toArray();
+    $descriptor = $config->database->master->toArray();
+
     if ($config->database->get('initialize', false)) {
         unset($descriptor['dbname']);
     }
     $db = new Db($descriptor);
 
+    // SQL実行ログを出力する
+    if (APPLICATION_ENV !== 'prod') {
+        $eventsManager = new \Phalcon\Events\Manager();
+        $eventsManager->attach('db', function ($event, $connection) use ($di) {
+            if ($event->getType() == 'beforeQuery') {
+                $di->get('profiler')->startProfile($connection->getSQLStatement());
+            }
+            if ($event->getType() == 'afterQuery') {
+                $di->get('profiler')->stopProfile();
+            }
+        });
+        $db->setEventsManager($eventsManager);
+    }
+    return $db;
+});
+
+$di->set('dbSlave', function () use ($di) {
+    if (!$di->has('config')) {
+        return;
+    }
+    $config = $di->get('config');
+
+    $descriptor = $config->database->slave->toArray();
+
+    if ($config->database->get('initialize', false)) {
+        unset($descriptor['dbname']);
+    }
+    $db = new Db($descriptor);
+
+    // SQL実行ログを出力する
     if (APPLICATION_ENV !== 'prod') {
         $eventsManager = new \Phalcon\Events\Manager();
         $eventsManager->attach('db', function ($event, $connection) use ($di) {
